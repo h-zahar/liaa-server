c9fd657f1c64b31aacf3cc731dcb981a07dc9638
h-zahar <hz.rifat@gmail.com>
2023-10-04 21:58:47 +0600

    Integrated Google Speech API

--- app/service.py
+++ app/service.py
@@ -1,43 +1,58 @@
+from google.cloud import speech
+from google.oauth2 import service_account
+import io
 import os
 import openai
 import requests
 import json
 from fastapi.encoders import jsonable_encoder
+import base64
 
 MAX_LENGTH = 10000
 
 # Load API key from an environment variable or secret management service
+
+
 def load_and_check_api_key():
     if os.getenv("OPENAI_API_KEY") not in [None, ""]:
         openai.api_key = os.getenv("OPENAI_API_KEY")
         return True
     return False
 
+
 def validate_input(user_input):
     if len(user_input) > MAX_LENGTH:
-        return { 'status': False, 'msg': f"Max Length Allowed: {MAX_LENGTH}" }
+        return {'status': False, 'msg': f"Max Length Allowed: {MAX_LENGTH}"}
     if (user_input == ""):
-        return { 'status': False, 'msg': "Input cannot be empty" }
-    return { 'status': True, 'msg': "Input is valid" }
+        return {'status': False, 'msg': "Input cannot be empty"}
+    return {'status': True, 'msg': "Input is valid"}
+
 
 def show_snippet(response):
     report = response["choices"][0]["message"]["content"]
     print(f"\nReport: {report}")
-    return { 'status': True, 'msg': report }
+    return {'status': True, 'msg': report}
+
 
 def generate_report(input: str):
     isValid = validate_input(input)
     if (isValid['status'] == False):
-        return { 'status': False, 'msg': isValid['msg'] }
+        return {'status': False, 'msg': isValid['msg']}
 
     is_key = load_and_check_api_key()
     if is_key == False:
-        return { 'status': False, 'msg': "API Key not found" }
+        return {'status': False, 'msg': "API Key not found"}
 
     print(f"Value: {input}")
     print("Generating report...")
-    
+
     convo = input
+
+    file = open("conversation.txt", "r")
+
+    convo = file.read()
+
+    file.close()
 
     prompt = f"""{convo}
 
@@ -63,15 +78,16 @@ def generate_report(input: str):
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{
         "role": "system",
         "content": "You're a medical note scribe"
-    }, 
-    {
+    },
+        {
         "role": "user",
         "content": prompt
     }])
 
     return show_snippet(response)
 
-def generate_text():
+
+def generate_text_wh():
     headers = {
         'accept': 'application/json',
         'x-gladia-key': os.getenv("GLADIA_API_KEY"),
@@ -88,7 +104,8 @@ def generate_text():
         'diarization_max_speakers': 2
     }
 
-    response = requests.post('https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
+    response = requests.post(
+        'https://api.gladia.io/audio/text/audio-transcription/', headers=headers, files=files)
     jsoned = response.json()
     data = jsonable_encoder(jsoned)
 
@@ -97,5 +114,64 @@ def generate_text():
         t_data.append(dict(tr=arr["transcription"], sp=arr["speaker"]))
 
     print(t_data)
-    
+
     return t_data
+
+
+def generate_text():
+    try:
+        client_file = 'sa/sa_liaa_ai.json'
+        credentials = service_account.Credentials.from_service_account_file(
+            client_file)
+        client = speech.SpeechClient(credentials=credentials)
+
+        # config = {
+        #     "languageCode": "en-US",
+        #     "encoding": "LINEAR16",
+        #     "sample_rate_hertz": "8000",
+        # }
+
+        with open("audio/test-2.wav", "rb") as audio_file:
+            content = audio_file.read()
+
+        audio = speech.RecognitionAudio(content=content)
+
+        # print(audio)
+
+        config = speech.RecognitionConfig(
+            language_code='en-US',
+            encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+            sample_rate_hertz=48000
+        )
+
+        response = client.recognize(config=config, audio=audio)
+
+        # print(response)
+
+        data = []
+
+        for result in response.results:
+            # The first alternative is the most likely one for this portion.
+            data.append(dict(tr=result.alternatives[0].transcript))
+
+        # audio = {
+        #     "content": content.decode('utf-8')
+        # }
+
+        # headers = {
+        #     "accept": "application/json",
+        #     "X-goog-api-key": ""
+        # }
+
+        # json = {"config": config, "audio": audio}
+
+        # response = requests.post(
+        #     'https://speech.googleapis.com/v1/speech:recognize', headers=headers, json=json)
+
+        # jsoned = response.json()
+        # data = jsonable_encoder(jsoned)
+
+        return data
+
+    except Exception as e:
+        print(e)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from app.service import generate_report, generate_text
+from app.service import generate_report, generate_text, generate_text_wh
 
 app = FastAPI()
 
@@ -19,20 +19,31 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.get("/")
 async def read_root():
-    return {"Hello, Hit": "https://localhost:3000/report/?input=[conversation]"}
+    return {"Hello, Hit": "https://localhost:5000/report?input=[conversation]"}
+
 
 @app.get("/report")
 async def get_generate_report(input: str):
     content = generate_report(input)
     if content['status'] == False:
         raise HTTPException(status_code=404, detail=content['msg'])
-    return { 'status_code': 200, 'content': content['msg'] }
+    return {'status_code': 200, 'content': content['msg']}
+
 
 @app.get("/convert")
 async def get_generate_text():
 
     content = generate_text()
 
-    return { 'status_code': 200, 'content': content }
+    return {'status_code': 200, 'content': content}
+
+
+@app.get("/convert-wh")
+async def get_generate_text():
+
+    content = generate_text_wh()
+
+    return {'status_code': 200, 'content': content}
